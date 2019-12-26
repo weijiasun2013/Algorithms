0b293b9099d9cf5f84d598958154877c245a1f37
Weiwei Wu <weiweiwu@Weiweis-MacBook-Pro.local>
2019-12-26 14:50:25 -0500

    add another solution

--- linkedlist/addition/addition.py
+++ linkedlist/addition/addition.py
@@ -1,153 +1,89 @@
 
-'''
-addition of two numbersthat are represented by a linked list
-1 -> 2 -> 3 -> null: 124
-9 -> 8 -> null: 98
-result: 2 -> 2 -> 2 -> null
-'''
+# linkedlist addition
 
-class Node(object):
+class node(object):
     def __init__(self,value):
-        self.val = value
+        self.value = value
         self.next = None
 
-def show(head):
-    h = head
-    rslt = ''
-    while h:
-        rslt += str(h.val) + ' -> '
-        h = h.next
-    rslt += 'null'
-    print(rslt)
+def display(node):
+    n = node
+    s = ''
+    while n:
+        s += str(n.value) + '->'
+        n = n.next
+    print(s)
 
-def addition(head1, head2):
-    if head1 is None:
-        return head2
-    elif head2 is None:
-        return head1
-
-    r1 = reverse_linked_list(head1)
-    r2 = reverse_linked_list(head2)
-
-    rslt,p3 = None,None
-    p1,p2 = r1,r2
-    ans = 0
-    while p1 and p2:
-        v = p1.val + p2.val + ans
-        ans = 0
-        if v > 9:
-            v -= 10
-            ans = 1
-        if rslt is None:
-            rslt = Node(v)
-            p3 = rslt
-        else:
-            p3.next = Node(v)
-            p3 = p3.next
-        p1,p2 = p1.next, p2.next
-
-    def fun(pSrc,pRslt,a):
-        p = pSrc
-        ans = a
-        while p:
-            v = p.val + ans
-            ans = 0
-            if v > 9:
-                v -= 10
-                ans = 1
-            pRslt.next = Node(v)
-            pRslt = pRslt.next
-            if p.next is None:
-                if ans > 0:
-                    pRslt.next = Node(ans)
-                    pRslt = pRslt.next
-            p = p.next
-    if p1:
-        fun(p1,p3,ans)
-    elif p2:
-        fun(p2,p3,ans)
-    elif ans > 0:
-        p3.next = Node(ans)
-        p3 = p3.next
-
-    rslt = reverse_linked_list(rslt)
-    return rslt
-
-def reverse_linked_list(head):
-    if head is None or head.next is None:
-        return head
-
-    h,cur = head,head.next
-    h.next = None
+def reverse(node):
+    head, cur = node, node.next
+    head.next = None
     while cur:
-        _next = cur.next
-        cur.next = h
-        h = cur
-        cur = _next
+        next = cur.next
+        cur.next = head
+        head = cur
+        cur = next
+    return head
 
-    return h
+def helper(val1,val2,flag,head,cursor):
+    v = val1 + val2 + flag
+    if v > 9:
+        tmp = node(v-10)
+        flag = 1
+    else:
+        tmp = node(v)
+        flag = 0
 
-def test1():
-    print('test1------------------')
-    n1,n2 = None,None
-    show(n1)
-    show(n2)
-    r = addition(n1,n2)
-    show(r)
-
-def test2():
-    print('test2------------------')
-    n1 = Node(1)
-    n2 = None
-    show(n1)
-    show(n2)
-    r = addition(n1,n2)
-    show(r)
-
-def test3():
-    print('test3------------------')
-    n1 = None
-    n2 = Node(1)
-    show(n1)
-    show(n2)
-    r = addition(n1,n2)
-    show(r)
-
-
-def test4():
-    print('test4------------------')
-    n1 = Node(9)
-    n2 = Node(9)
-
-    show(n1)
-    show(n2)
-    r = addition(n1,n2)
-    show(r)
-
-def test5():
-    print('test4------------------')
-    n1 = Node(2)
-    n2 = Node(9)
-    n3 = Node(9)
-
-    n1.next = n2
-    n2.next = n3
-
-    n11 = Node(1)
-    n12 = Node(8)
-    n11.next = n12
+    if head is None:
+        head = tmp
+        cursor = head
+    else:
+        cursor.next = tmp
+        cursor = tmp
     
-    show(n1)
-    show(n11)
-    r = addition(n1,n11)
-    show(r)
+    return head,cursor,flag
 
-def main():#
-    test1()
-    test2()
-    test3()
-    test4()
-    test5()
+def addition(node1,node2):
+    n1 = reverse(node1)
+    n2 = reverse(node2)
+    head,cursor = None, None
+    flag = 0
 
-if __name__ == '__main__':
+    c1,c2 = n1,n2
+    while c1 and c2:
+        head,cursor,flag = helper(c1.value,c2.value,flag,head,cursor)
+        c1 = c1.next
+        c2 = c2.next
+
+    if c1:
+        c = c1
+        while c:
+            head,cursor,flag = helper(c.value,0,flag,head,cursor)
+            c = c.next
+    elif c2:
+        c = c2
+        head,cursor,flag = helper(0,c.value,flag,head,cursor)
+        c = c.next
+
+    if flag:
+        head,cursor,flag = helper(0,0,flag,head,cursor)
+
+    return reverse(head)
+
+def main():
+    n1 = node(1)
+    n2 = node(2)
+    n3 = node(3)
+    n1.next,n2.next = n2, n3
+    display(n1)
+
+    n11 = node(9)
+    n12 = node(8)
+    n13 = node(7)
+    n11.next,n12.next = n12, n13
+    display(n11)
+    ans = addition(n1,n11)
+    display(ans)
+    
+if __name__ == "__main__":
     main()
+
---
+++ linkedlist/delete_last_occurance/delete_last_occurance_2.py
@@ -0,0 +1,90 @@
+
+
+# reserve first, then delete the iirst occurance, then reverse
+
+class Node(object):
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+
+def display(node):
+    n = node
+    ans = ''
+    while n:
+        ans += str(n.val) + ' -> '
+        n = n.next
+    ans += 'null'
+    print(ans)
+
+def reverse(node):
+    if node is None:
+        return node
+
+    head,cur = node, node.next
+    head.next = None
+    while cur:
+        tmp = cur.next
+        cur.next = head
+        head = cur
+        cur = tmp
+
+    return head
+
+def delete_last_occurance(node,item):
+    _node = reverse(node)
+    if _node.val == item:
+        _node = _node.next
+        return reverse(_node)
+
+    pre,cur = _node,_node.next
+    while cur:
+        if cur.val == item:
+            pre.next = cur.next
+            break
+        cur = cur.next
+        pre = pre.next
+
+    return reverse(_node)
+
+def test1():
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(1)
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+
+
+    head = delete_last_occurance(n1,1)
+    display(head)
+
+def test2():
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(3)
+    n6 = Node(5)
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+    n5.next = n6
+
+    display(n1)
+    display(delete_last_occurance(n1,3))
+
+def test3():
+    n1 = Node(1)
+    display(n1)
+    display(delete_last_occurance(n1,1))
+
+def main():
+    test1()
+    test2()
+    test3()
+
+if __name__ == '__main__':
+    main()
+
