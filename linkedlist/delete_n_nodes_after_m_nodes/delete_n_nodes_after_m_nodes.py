210baa7d138ec193c47e1a14494f53ef93d30070
Weiwei Wu <weiweiwu@Weiweis-MacBook-Pro.local>
2019-12-27 09:59:46 -0500

    fixed up a bug

--- linkedlist/delete_n_nodes_after_m_nodes/delete_n_nodes_after_m_nodes.py
+++ linkedlist/delete_n_nodes_after_m_nodes/delete_n_nodes_after_m_nodes.py
@@ -1,95 +1,71 @@
 
-'''
-delete n nodes after m nodes in linked list
-'''
+# reserve first, then delete the iirst occurance, then reverse
 
 class Node(object):
-    def __init__(self,value):
-        self.val = value
+    def __init__(self,val):
+        self.val = val
         self.next = None
 
-def show(head):
-    h = head
-    rslt = ''
-    while h:
-        rslt += str(h.val) + ' -> '
-        h = h.next
-    rslt += 'null'
-    print(rslt)
+def display(node):
+    n = node
+    ans = ''
+    while n:
+        ans += str(n.val) + ' -> '
+        n = n.next
+    ans += 'null'
+    print(ans)
 
-def delete_n_nodes_after_m_nodes(head,m,n):
-    c1,c2 = m,n
-    p,prev = head,None
-    while c1 > 0:
-        c1 -= 1
-        p = p.next
-        if prev is None:
-            prev = head
+def delete_n_nodes_after_m_nodes(node,m,n):
+    if m == 0:
+        return None
+    if n == 0:
+        return node
+
+    p1 = node
+    flag = True
+    sum = m - 1
+    while p1:
+        display(node)
+        if flag:
+            if sum > 0:
+                sum -= 1
+                p1 = p1.next
+            elif sum == 0:
+                flag = False
+                sum = n
         else:
-            prev = prev.next
-    p2 = p
-    while c2 > 0:
-        c2 -= 1
-        p2 = p2.next
+            if sum > 0:
+                if p1 and p1.next:
+                    tmp = p1.next
+                    p1.next = tmp.next
+                    sum -= 1
+                else:
+                    break
+            else:
+                flag = True
+                sum = m - 1
+                p1 = p1.next
 
-    if prev is None:
-        return p2
-    else:
-        prev.next = p2
-        return head
+    return node
 
-def test1():
-    print('test1------------------')
-    n1 = Node(1)
-    n2 = Node(2)
-    n3 = Node(3)
-    n4 = Node(4)
-    n5 = Node(5)
-
-    n1.next = n2
-    n2.next = n3
-    n3.next = n4
-    n4.next = n5
-
-    show(n1)
-    r = delete_n_nodes_after_m_nodes(n1,2,2)
-    show(r)
-
-def test2():
-    print('test2------------------')
+def main():
     n1 = Node(1)
     n2 = Node(2)
     n3 = Node(3)
     n4 = Node(4)
     n5 = Node(5)
     n6 = Node(6)
-
+    n7 = Node(7)
     n1.next = n2
     n2.next = n3
     n3.next = n4
     n4.next = n5
     n5.next = n6
+    n6.next = n7
+    rslt = delete_n_nodes_after_m_nodes(n1,2,1)
+    display(rslt)
 
-    show(n1)
-    r = delete_n_nodes_after_m_nodes(n1,0,3)
-    show(r)
-
-def test3():
-    print('test3------------------')
-    n1 = Node(1)
-    n2 = Node(2)
-    n3 = Node(3)
-
-    n1.next = n2
-    n2.next = n3
-    show(n1)
-    r = delete_n_nodes_after_m_nodes(n1,0,3)
-    show(r)
-
-def main():#
-    test1()
-    test2()
-    test3()
 
 if __name__ == '__main__':
     main()
+
