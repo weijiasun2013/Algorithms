fc906abe6101f18cba4ed93a8e7a9418d2968980
Weiwei Wu <weiweiwu@Weiweis-MacBook-Pro.local>
2019-12-27 10:26:12 -0500

    fixed up a bug

--- linkedlist/delete_middle/delete_middle.py
+++ linkedlist/delete_middle/delete_middle.py
@@ -1,101 +1,86 @@
 
-'''
-delete middle of linked list
-'''
+# reserve first, then delete the iirst occurance, then reverse
 
 class Node(object):
-    def __init__(self,value):
-        self.val = value
+    def __init__(self,val):
+        self.val = val
         self.next = None
 
-def show(head):
-    h = head
-    rslt = ''
-    while h:
-        rslt += str(h.val) + ' -> '
-        h = h.next
-    rslt += 'null'
-    print(rslt)
+def display(node):
+    if node is None:
+        print('null')
+        return
 
-def delete_middle(head):
-    if head is None:
+    n = node
+    ans = ''
+    while n:
+        ans += str(n.val) + ' -> '
+        n = n.next
+    ans += 'null'
+    print(ans)
+
+def delete_middle(node):
+    if node == Node or node.next == None:
         return None
-    if head.next is None:
-        return None
 
-    p1,p2,prev = head,head,head
-    first = True
-    while p1 and p1.next:
-        if first == True:
-            first = False
-        else:
-            prev = prev.next
-        p1 = p1.next.next
-        p2 = p2.next
+    p1,p2 = node,node.next.next
+    while p2 and p2.next:
+        p2 = p2.next.next
+        p1 = p1.next
+    p1.next = p1.next.next
 
-    prev.next = p2.next
-    return head
+    return node
 
 def test1():
-    n1 = None
-    show(n1)
-    r = delete_middle(n1)
-    show(r)
-
-def test2():
-    n1 = Node(1)
-    show(n1)
-    r = delete_middle(n1)
-    show(r)
-
-def test3():
-    n1 = Node(1)
-    n2 = Node(2)
-    n1.next = n2
-    show(n1)
-    r = delete_middle(n1)
-    show(r)
-
-def test4():
     n1 = Node(1)
     n2 = Node(2)
     n3 = Node(3)
     n4 = Node(4)
     n5 = Node(5)
-
     n1.next = n2
     n2.next = n3
     n3.next = n4
     n4.next = n5
+    rslt = delete_middle(n1)
+    display(rslt)
 
-    show(n1)
-    r = delete_middle(n1)
-    show(r)
+def test2():
+    rslt = delete_middle(Node)
+    display(rslt)
+
+def test3():
+    n1 = Node(1)
+    rslt = delete_middle(n1)
+    display(rslt)
+
+def test4():
+    n1 = Node(1)
+    n2 = Node(2)
+    n1.next = n2
+    rslt = delete_middle(n1)
+    display(rslt)
 
 def test5():
     n1 = Node(1)
     n2 = Node(2)
     n3 = Node(3)
     n4 = Node(4)
-    n5 = Node(5)
-    n6 = Node(6)
 
     n1.next = n2
     n2.next = n3
     n3.next = n4
-    n4.next = n5
-    n5.next = n6
 
-    show(n1)
-    r = delete_middle(n1)
-    show(r)
+    rslt = delete_middle(n1)
+    display(rslt)
 
-def main():#
+def main():
     test1()
     test2()
     test3()
     test4()
     test5()
 
+
 if __name__ == '__main__':
     main()
+
