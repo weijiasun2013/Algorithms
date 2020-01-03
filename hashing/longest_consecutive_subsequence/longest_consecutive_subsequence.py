4c502cbddd22ac3cf243a3d5b4021bf9052cc603
Weiwei Wu <weiweiwu@Weiweis-MacBook-Pro.local>
2020-01-03 14:48:41 -0500

    refreshing coding

--- hashing/longest_consecutive_subsequence/longest_consecutive_subsequence.py
+++ hashing/longest_consecutive_subsequence/longest_consecutive_subsequence.py
@@ -1,47 +1,43 @@
 
-# need to improve
 
 import sys
 
 def longest_consecutive_subsequence(arr):
-    # Given an array of integers, find the length of the longest sub-sequence
-    # such that elements in the subsequence are consecutive integers, the consecutive
-    # numbers can be in any order.
-
-    d = dict()
-    _min, _max = sys.maxsize, -sys.maxsize
+    dct = dict()
+    _low,_high = sys.maxsize,-sys.maxsize
     for ele in arr:
-        d[ele] = 1
-        _max = max(_max,ele)
-        _min = min(_min, ele)
+        dct[ele] = -1
+        _low = min(ele, _low)
+        _high = max(ele,_high)
 
-    aux = dict()
-    length,low,high = 0,None,None
-    for ele in d.keys():
-        _low,_high = ele,ele + 1
-        cnt = 1
-        while _high <= _max:
-            if _high in aux:
-                aux[_low] = cnt + aux[_high]
-                if length < aux[_low]:
-                    length = aux[_low]
-                    low,high = _low, _low + aux[_low] - 1
-                break
-            elif _high in d:
-                cnt += 1
-                aux[_low] = cnt
-                if length < aux[_low]:
-                    length = aux[_low]
-                    low,high = _low,_low + aux[_low] - 1
-            else:
-                break
-            _high += 1
+    _max,_small = 0,0
+    for k,v in dct.items():
+        if v == -1:
+            dct[k] = 0
+            _length = 1
 
-    return length,(low,high)
+            cur = k + 1
+            while cur in dct and cur <= _high:
+                _length += 1
+                dct[cur] = 0
+                cur += 1
+
+            cur = k - 1
+            while cur in dct and cur >= _low:
+                _length += 1
+                dct[cur] = 0
+                cur -= 1
+
+            if _length > _max:
+                _max = _length
+                _small = cur + 1
+
+    return _max, _small
 
 def main():
-    length,rng = longest_consecutive_subsequence([1, 19, 3, 10, 4, 20, 2,21,23,22])
-    print(length,rng)
+    _max,_small = longest_consecutive_subsequence([2, 9, 3, 10, 4, 20, 1])
+    print('length:', _max, "start num: ", _small)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
