6871ce43c007095db50b5fa51c8c560e7e73e471
Weiwei Wu <weiweiwu@Weiweis-MacBook-Pro.local>
2020-01-05 11:46:30 -0500

    refreshing coding

--- linkedlist/delete_last_occurance/delete_last_occurance.py
+++ linkedlist/delete_last_occurance/delete_last_occurance.py
@@ -1,127 +1,93 @@
 
-'''
-delete last occurance of an item in linked list
-'''
+
+# reserve first, then delete the iirst occurance, then reverse
 
 class Node(object):
-    def __init__(self,value):
-        self.val = value
+    def __init__(self,val):
+        self.val = val
         self.next = None
 
-def show(head):
-    h = head
-    rslt = ''
-    while h:
-        rslt += str(h.val) + ' -> '
-        h = h.next
-    rslt += 'null'
-    print(rslt)
+def display(node):
+    n = node
+    ans = ''
+    while n:
+        ans += str(n.val) + ' -> '
+        n = n.next
+    ans += 'null'
+    print(ans)
 
-def delete_last_occurance(head,item):
-    if head is None:
-        return head
+def reverse(node):
+    if node is None:
+        return node
 
-    p1,p2,prev1,prev2 = head,None,None,None
-    flag1 = True    # run p1 if True, else run p2
-    while (p1 and flag1) or (p2 and flag1 == False):
-        if flag1:
-            if p1.val == item:
-                flag1 = False
-                p2 = p1.next
-                prev2 = p1
-            else:
-                p1 = p1.next
-                if prev1 is None:
-                    prev1 = head
-                else:
-                    prev1 = prev1.next
-        else:   #run p2
-            if p2.val == item:
-                flag1 = True
-                p1 = p2.next
-                prev1 = p2
-            else:
-                p2 = p2.next
-                if prev2 is None:
-                    prev2 = head
-                else:
-                    prev2 = prev2.next
-    if flag1:
-        if p2 and prev2:
-            prev2.next = p2.next
-    else:
-        if prev1:
-            prev1.next = p1.next
-        else:
-            head = p1.next
+    head,cur = node, node.next
+    head.next = None
+    while cur:
+        tmp = cur.next
+        cur.next = head
+        head = cur
+        cur = tmp
+
     return head
 
+def delete_last_occurance(node,item):
+    _node = reverse(node)
+    if _node.val == item:
+        _node = _node.next
+        return reverse(_node)
+
+    pre,cur = _node,_node.next
+    while cur:
+        if cur.val == item:
+            pre.next = cur.next
+            break
+        cur = cur.next
+        pre = pre.next
+
+    return reverse(_node)
+
 def test1():
-    print('test1------------------')
-    n1 = None
-    show(n1)
-    r = delete_last_occurance(n1,1)
-    show(r)
-
-def test2():
-    print('test2------------------')
-    n1 = Node(1)
-    show(n1)
-    r = delete_last_occurance(n1,1)
-    show(r)
-
-def test3():
-    print('test3------------------')
     n1 = Node(1)
     n2 = Node(2)
-    n3 = Node(1)
-    n4 = Node(2)
-    n5 = Node(1)
-
+    n3 = Node(3)
+    n4 = Node(1)
     n1.next = n2
     n2.next = n3
     n3.next = n4
-    n4.next = n5
+    display(n1)
 
-    show(n1)
-    r = delete_last_occurance(n1,1)
-    show(r)
+    head = delete_last_occurance(n1,1)
+    display(head)
+    print("")
 
-def test4():
-    print('test4------------------')
+def test2():
     n1 = Node(1)
     n2 = Node(2)
     n3 = Node(3)
     n4 = Node(4)
-    n5 = Node(5)
-    n6 = Node(6)
-
+    n5 = Node(3)
+    n6 = Node(5)
     n1.next = n2
     n2.next = n3
     n3.next = n4
     n4.next = n5
     n5.next = n6
 
-    show(n1)
-    r = delete_last_occurance(n1,6)
-    show(r)
+    display(n1)
+    display(delete_last_occurance(n1,3))
+    print("")
 
-def test5():
-    print('test5------------------')
+def test3():
     n1 = Node(1)
-    n2 = Node(2)
+    display(n1)
+    display(delete_last_occurance(n1,1))
+    print("")
 
-    n1.next = n2
-    show(n1)
-    r = delete_last_occurance(n1,3)
-    show(r)
-
-def main():#
+def main():
     test1()
     test2()
     test3()
-    test4()
-    test5()
 
 if __name__ == '__main__':
     main()
+
